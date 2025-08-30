57b4cccc2ec9eacc14f677661960d5f3daae0716
javadocq <han18264@naver.com>
2025-08-31 02:12:40 +0900

    chore: color system setting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <p className="text-h2">안녕</p>
+    <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-white">
+      <p className="text-h2 text-shadow-8">안녕</p>
     </div>
   );
 }
