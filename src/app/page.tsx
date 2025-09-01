e40c39f5bfa6199f7fe29e01ea82733427363c29
javadocq <han18264@naver.com>
2025-09-02 01:08:09 +0900

    feat: header ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Header } from "@/shared/ui";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-white">
-      <p className="text-h2 text-shadow-8">안녕</p>
+      <Header />
     </div>
   );
 }
