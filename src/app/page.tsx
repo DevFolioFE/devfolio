f87ab6c45d53074e237eee15c494574af20ccc93
javadocq <han18264@naver.com>
2025-09-02 21:13:55 +0900

    feat: link-preview ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import { InfoBlock } from "@/features/home/ui/InfoBlock";
+import { LinkPreview } from "@/features/home";
+import TestSvg from "@/assets/icons/test_preview.svg";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-white">
-      <InfoBlock type="A" />
-      <InfoBlock type="B" />
+      <LinkPreview
+        imageSrc={TestSvg}
+        title="Profile Header"
+        description="Showcase your skills and experience with a customizable profile header."
+      />
     </div>
   );
 }
---
+++ src/features/home/ui/LinkPreview.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+interface LinkPreviewProps {
+  imageSrc: string;
+  title: string;
+  description: string;
+}
+
+export function LinkPreview({
+  imageSrc,
+  title,
+  description,
+}: LinkPreviewProps) {
+  return (
+    <article className="flex flex-col w-[301.33px] h-[272px] items-start gap-4 rounded-lg cursor-pointer">
+      <Image
+        alt="test"
+        src={imageSrc}
+        className="w-full h-[169px] rounded-lg"
+      />
+      <section className="flex flex-col items-start self-stretch w-full">
+        <div className="flex flex-col items-start w-full">
+          <p className="text-body-lg-medium">{title}</p>
+        </div>
+        <div className="flex flex-col items-start w-full">
+          <p className="text-body-md-regular text-[#61758A]">{description}</p>
+        </div>
+      </section>
+    </article>
+  );
+}
